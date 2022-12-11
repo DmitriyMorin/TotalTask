3b71a03807f6292eb759aa8b4772c42ff9b9eb50
Dmitrii Morin <dimka.morin@gmail.com>
2022-12-11 17:27:02 +0300

    add Library

---
+++ Library.cs
@@ -0,0 +1,24 @@
+public static class Library
+{
+    public static void SecondArray(string[] array1, string[] array2)
+ {
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+ }
+public static void PrintArray(string[] array)
+ {
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+ }
+
+}
